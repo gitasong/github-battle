a851c46d0aef73b2e23b2216f300a472fae2d101
Nicole Freed <nicole.freed@gmail.com>
2018-11-12 13:56:00 -0800

    build out simple demo App component

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const React = require('react');
+
+// this.state
+// lifecycle methods
+// UI**
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
